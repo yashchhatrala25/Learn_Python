7667be296712d4c61b251a987c23ca821122a81d
yashchhatrala25 <yashchhatrala25@gmail.com>
2025-06-19 22:20:06 +0530

    assignment_1 complete

---
+++ Assignment_1/01.py
@@ -0,0 +1,6 @@
+num_1 = int(input("Enter First Number: "))
+num_2 = int(input("Enter Second Number: "))
+print("Addition", num_1 + num_2)
+print("Substraction", num_1 - num_2)
+print("Multiplication", num_1 * num_2)
+print("Division", num_1 / num_2)
